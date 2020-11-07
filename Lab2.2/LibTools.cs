08f5d246320c9ac3aa50e423f376b5efb69e79fd
leeblle <73112790+leeblle@users.noreply.github.com>
2020-11-07 14:52:08 +0500

    Добавьте файлы проекта.

---
+++ Lab2.2/LibTools.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lab2._2
+{
+	public class LibTools
+	{
+		[Flags]
+		private enum ErrorModes : uint
+		{
+			SYSTEM_DEFAULT = 0x0,
+			SEM_FAILCRITICALERRORS = 0x0001,
+			SEM_NOALIGNMENTFAULTEXCEPT = 0x0004,
+			SEM_NOGPFAULTERRORBOX = 0x0002,
+			SEM_NOOPENFILEERRORBOX = 0x8000
+		}
+
+		public double TheFunc(double x) => _theFunc(x);
+		public string FuncName() => Marshal.PtrToStringAnsi(_funcName());
+
+		private readonly _TheFuncDelegate _theFunc;
+		private readonly _FuncNameDelegate _funcName;
+
+		[DllImport("kernel32.dll")] private static extern IntPtr LoadLibrary(string path);
+		[DllImport("kernel32.dll")] private static extern ErrorModes SetErrorMode(ErrorModes modes);
+		[DllImport("kernel32.dll")] private static extern IntPtr GetProcAddress(IntPtr hLib, string name);
+
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl)] private delegate double _TheFuncDelegate(double x);
+		[UnmanagedFunctionPointer(CallingConvention.StdCall)] private delegate IntPtr _FuncNameDelegate();
+
+		public LibTools(string path)
+		{
+			IntPtr hLib = GetLibrary(path);
+			if (hLib != IntPtr.Zero)
+			{
+				_theFunc = GetFunc<_TheFuncDelegate>("TheFunc", hLib);
+				_funcName = GetFunc<_FuncNameDelegate>("FuncName", hLib);
+			}
+			else
+				throw new Exception();
+		}
+
+		private IntPtr GetLibrary(string path)
+		{
+			var oldMode = SetErrorMode(ErrorModes.SEM_FAILCRITICALERRORS);
+			try
+			{
+				return LoadLibrary(path);
+			}
+			finally
+			{
+				SetErrorMode(oldMode);
+			}
+		}
+
+		private T GetFunc<T>(string funcName, IntPtr hLib) where T : class
+		{
+			IntPtr address = GetProcAddress(hLib, funcName);
+			if (address != IntPtr.Zero)
+				return Marshal.GetDelegateForFunctionPointer(address, typeof(T)) as T;
+			else
+				throw new Exception();
+		}
+	}
+}
---
+++ Lab2.2/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Linq;
+
+namespace Lab2._2
+{
+	public partial class MainWindow : Window
+	{
+		private readonly List<LibTools> methods = new List<LibTools>();
+
+		public MainWindow() => InitializeComponent();
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			string[] pathsDll = Directory.GetFiles("Plugins", "*.dll", SearchOption.TopDirectoryOnly);
+			foreach (var path in pathsDll)
+			{
+				try
+				{
+					methods.Add(new LibTools(path));
+				}
+				catch { }
+			}
+
+			foreach (var method in methods)
+				ComboBoxFuncs.Items.Add(method.FuncName());
+			ComboBoxFuncs.Items.Add("Все");
+		}
+
+		private void ButtonApplyFunc_Click(object sender, RoutedEventArgs e)
+		{
+			ChartTheFunc.Series.Clear();
+			ChartValues<ObservablePoint> points = new ChartValues<ObservablePoint>();
+			Func<string> funcName = null;
+			Func<double, double> theFunc = null;
+			var nameMethod = ComboBoxFuncs.Text;
+			if (nameMethod == "Все")
+			{
+				foreach (var method in methods)
+				{
+					points = new ChartValues<ObservablePoint>();
+
+					funcName = method.FuncName;
+					theFunc = method.TheFunc;
+
+					for (double x = 0; x <= 10; x += 0.1)
+					{
+						//points.Add(new ObservablePoint(x, theFunc(x)));
+						if (funcName() == "y=12x+sin(x/2)")
+						{
+							points.Add(new ObservablePoint(x, theFunc(x * 40)));
+						}
+						else if (funcName().Contains("cos"))
+						{
+							points.Add(new ObservablePoint(x, theFunc(x)));
+						}
+						else
+						{
+							points.Add(new ObservablePoint(x, theFunc(x)));
+						}
+					}
+
+					ChartTheFunc.Series.Add(new LineSeries
+					{
+						Values = points,
+						Title = funcName()
+					});
+				}
+			}
+			else
+			{
+				foreach (var method in from method in methods
+									   where nameMethod == method.FuncName()
+									   select method)
+				{
+					funcName = method.FuncName;
+					theFunc = method.TheFunc;
+				}
+
+				for (double x = 0; x <= 10; x += 0.1)
+					points.Add(new ObservablePoint(x, theFunc(x)));
+
+				ChartTheFunc.Series.Clear();
+				ChartTheFunc.Series.Add(new LineSeries
+				{
+					Values = points,
+					Title = funcName()
+				});
+			}
+		}
+	}
+}
